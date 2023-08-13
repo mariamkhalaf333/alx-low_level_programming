62c4c520482514d2f65fdbc075995e01c9959a0d
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-13 11:34:24 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function 
+ * main - Entry point 
  *
  * Return: Alawys 0 (Success)
 */
-
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
