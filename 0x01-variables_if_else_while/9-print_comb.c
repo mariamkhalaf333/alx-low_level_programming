6138436c64ab12eba8680a478a9a5ff010268445
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-14 16:47:09 -0700

    last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + 48);
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
