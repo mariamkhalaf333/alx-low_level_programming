1d2e244a00f3ce43c8fca449e4abc99e81dc4a18
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-14 15:28:04 -0700

    3

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%i", x);
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
