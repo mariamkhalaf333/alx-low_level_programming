6ef47f8a6c35551b34295f4186ef8e4cd3320822
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-15 14:55:47 -0700

    4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,22 @@
   */
 void times_table(void)
 {
-	int x, y, z, u, d;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		_putchar(48);
+		for (y = 1; y <= 9; y++)
 		{
+			_putchar('.');
+			putchar(' ');
 			z = x * y;
 
-			if (z > 9)
-			{
-				u = z % 10;
-				d = (z - u) / 10;
-
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
+			if (z <= 9)
+				_putchar('');
 			else
-			{
-				if (y != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-
-				_putchar(z + '0');
-			}
+			_putchar((z / 10) + 48);
+			_putchar((z % 10) + 48);
 		}
 
 		_putchar('\n');
