634116e188c615ed8b930a952e08b3d1fa757aa6
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-18 07:12:56 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@
 
 void print_diagonal(int n)
 {
-	int p, s;
+	int postn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (p = 1; p <= n; p++)
+		for (postn = 1; postn <= n; postn++)
 		{
-			for (s = 1; s <= p; s++)
+			for (space = 1; space <= postn; space++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
