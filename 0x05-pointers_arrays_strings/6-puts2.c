752efe8ccf5cc5032d90f220e6f6e3f754ac79fd
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-08-21 14:31:12 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a st
- * ring, starting with the first character, followed by a new line
+ * puts2 - function should print only one character out of two
+ * starting with the first one
  * @str: input
  * Return: print
 */
 void puts2(char *str)
 {
-	int l = 0;
+	int longi = 0;
 	int t = 0;
 	char *y = str;
 	int o;
@@ -15,9 +15,9 @@ void puts2(char *str)
 	while (*y != '\0')
 	{
 		y++;
-		l++;
+		longi++;
 	}
-	t = l - 1;
+	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
