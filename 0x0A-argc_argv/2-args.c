855ba68ed880799ee130741c36cd82e8ba3d7d4c
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-09-01 11:26:55 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
