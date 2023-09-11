e3127da7e2cfa8a24f092b01409ae83189053864
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-09-11 14:17:06 -0700

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * print_dog - prints a dog
+ * @d: the dog to print
+ *
+ * Return: void
+*/
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nill)");
+		prints("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	}
+}
