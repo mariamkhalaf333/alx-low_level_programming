e1e5767254e37671295b7b36a47f0904d755b19e
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-09-11 14:28:50 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,10 +1,11 @@
-#include"dog.h"
+#include "dog.h"
 #include <stdio.h>
+
 /**
  * print_dog - prints a dog
  * @d: the dog to print
  *
- * Return: void
+ * Return: void.
 */
 void print_dog(struct dog *d)
 {
