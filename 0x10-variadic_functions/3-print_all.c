db97a7c0e1fa88ab8b63d5482b6ea3df49e1bd3c
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-09-14 16:54:08 -0700

    3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,7 +4,7 @@
 
 /**
  * print_all - prints anything
- * @format: list of types
+ * @format: list of types of arguments passed to the function
 */
 void print_all(const char * const format, ...)
 {
