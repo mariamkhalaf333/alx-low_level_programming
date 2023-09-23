42339a07749ad7247f2087f6ca9f094639e1a31d
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-09-23 16:01:03 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,18 +9,35 @@
  *
  * Return: Always 0
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char *p = (char *)main;
-	int b;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	b = atoi(argv[1]);
-	if (b < 0)
-		printf("Error\n"), exit(2);
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-	while (b--)
-		printf("%02hhx%s", *p++, b ? " " : "\n");
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
