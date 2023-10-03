bec6a4a6fa0d51352ceff56be2576b5d4369b1be
mariamkhalaf333 <khalafmariam333@gmail.com>
2023-10-03 04:48:26 +0300

    0

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * main - chech the code
  *
- * Return: Always o
+ * Return: Always 0.
 */
 int main(int ac, char **av)
 {
@@ -16,8 +17,8 @@ int main(int ac, char **av)
 		 exit(1);
 	}
 	n = read_textfile(av[1], 114);
-	    printf("\n(printed chars: %li)\n", n);
-	        n = read_textfile(av[1], 1024);
-		    printf("\n(printed chars: %li)\n", n);
-		        return (0);
+	printf("\n(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("\n(printed chars: %li)\n", n);
+	return (0);
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 /**
  * read_textfile- Read text file print to STDOUT
  * @filename: text file being read
@@ -9,18 +8,17 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
+	if (!filename || !letters)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
-	free(buf);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-	return (w);
+	return (bytes);
 }
